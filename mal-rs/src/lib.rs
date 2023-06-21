3f066378a59b003c20f81f6cdfc6f25fc35304ce
jachii <jettymcfiggs@gmail.com>
2023-06-21 02:59:29 -0400

    Added oauth token endpoint

--- mal-rs/src/lib.rs
+++ mal-rs/src/lib.rs
@@ -3,6 +3,7 @@ pub mod manga;
 pub mod oauth;
 
 const OAUTH_URL: &'static str = "https://myanimelist.net/v1/oauth2/authorize";
+const OAUTH_TOKEN_URL: &'static str = "https://myanimelist.net/v1/oauth2/token";
 const API_URL: &'static str = "https://api.myanimelist.net/v2";
 
 #[cfg(test)]
