07a9e57c60602354ed77cabea463d4b4d395239b
jachii <jettymcfiggs@gmail.com>
2023-06-27 11:46:06 -0400

    Added macros for creating AnimeFields and MangaFields

---
+++ mal-rs/src/macros.rs
@@ -0,0 +1,25 @@
+#[macro_export]
+macro_rules! anime_fields {
+    ($($variant:path),* $(,)?) => {
+        {
+            let mut v = Vec::new();
+            $(
+                v.push($variant);
+            )*
+            AnimeFields(v)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! manga_fields {
+    ($($variant:path),* $(,)?) => {
+        {
+            let mut v = Vec::new();
+            $(
+                v.push($variant);
+            )*
+            MangaFields(v)
+        }
+    };
+}
