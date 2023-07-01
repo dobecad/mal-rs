fd94f39208ccf17d1a9034cfb3beb5615a1ce3eb
jachii <jettymcfiggs@gmail.com>
2023-07-01 14:44:03 -0400

    Use display for printing instead of debug

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -21,19 +21,19 @@ async fn main() {
     );
     let query = GetAnimeList::new("one".to_string(), Some(5), None, Some(&fields)).unwrap();
     let result = api_client.get_anime_list(query).await.unwrap();
-    println!("Result: {:?}", &result);
+    println!("Result: {}", &result);
 
     // Example iterating through pages
-    let result: Result<AnimeList, _> = api_client.next(&result).await;
-    println!("\nNext result: {:?}", &result);
+    let result: AnimeList = api_client.next(&result).await.unwrap();
+    println!("\nNext result: {}", &result);
 
-    let result: Result<AnimeList, _> = api_client.prev(&result.unwrap()).await;
-    println!("\nPrev result: {:?}", &result);
+    let result: AnimeList = api_client.prev(&result).await.unwrap();
+    println!("\nPrev result: {}", &result);
 
     // Mangag API example
     let api_client = MangaApiClient::from(&client_id);
     let fields = mal_rs::manga::all_fields();
     let query = GetMangaList::new("one".to_string(), Some(5), None, Some(&fields)).unwrap();
     let result = api_client.get_manga_list(query).await.unwrap();
-    println!("Result: {:?}", result);
+    println!("Result: {}", result);
 }
