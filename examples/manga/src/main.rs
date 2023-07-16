ed395b09875b89ae33a05096414edad6f01aa023
jachii <jettymcfiggs@gmail.com>
2023-07-16 16:13:54 -0400

    Adding manga api example

--- examples/manga/src/main.rs
+++ examples/manga/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy;
+use mal_rs::oauth::MalClientId;
+use mal_rs::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let client_id = MalClientId::from_env().unwrap();
+    let api_client = MangaApiClient::from(&client_id);
+    let nsfw = false;
+    let limit = Some(3);
 }
