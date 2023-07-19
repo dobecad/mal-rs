2a2f10adc5ec422b6a925da03886270b4befa7ff
jachii <jettymcfiggs@gmail.com>
2023-07-18 22:28:48 -0400

    Include builder pattern in example

--- examples/forum/src/main.rs
+++ examples/forum/src/main.rs
@@ -8,7 +8,6 @@ async fn main() {
 
     let client_id = MalClientId::from_env().unwrap();
     let api_client = ForumApiClient::from(&client_id);
-    let nsfw = false;
     let limit = Some(3);
 
     let topics = api_client.get_forum_boards().await;
@@ -22,17 +21,12 @@ async fn main() {
         println!("Forum topic detail: {}\n", response);
     }
 
-    let query = GetForumTopics::new(
-        nsfw,
-        Some("hello".to_string()),
-        None,
-        None,
-        None,
-        None,
-        limit,
-        None,
-    )
-    .unwrap();
+    let query = GetForumTopics::builder()
+        .q("hello")
+        .enable_nsfw()
+        .limit(5)
+        .build()
+        .unwrap();
     let response = api_client.get_forum_topics(&query).await;
     if let Ok(response) = response {
         println!("Forum topics: {}", response)
